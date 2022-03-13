85d78c328c833d1b04e6fb8c23ecece3a9d677a6
mezianefraga00 <mezianefraga00@gmail.com>
2022-03-12 19:01:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button2)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.editTextNumber2)
+        resultTextView.text = diceRoll.toString()
+    }
+}
